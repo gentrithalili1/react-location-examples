4cdbd401a567150a91175291941bc719b37160c6
Gentrit Halili <gentrithalili11@gmail.com>
2022-05-16 14:57:06 +0200

    Add devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Header from "./components/Header";
 import { Router, Outlet } from "@tanstack/react-location";
 import { location, routes } from "./routes";
-
+import { ReactLocationDevtools } from "@tanstack/react-location-devtools";
 function App() {
   return (
     <div className="App">
       <Router location={location} routes={routes}>
         <Header />
         <Outlet />
+        <ReactLocationDevtools initialIsOpen={true} />
       </Router>
     </div>
   );
