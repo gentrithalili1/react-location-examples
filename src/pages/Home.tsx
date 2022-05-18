efdd0c44cf4902ede22f4efc88f527b71c7078e9
Gentrit Halili <gentrithalili11@gmail.com>
2022-05-18 13:33:08 +0200

    Update devtools

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,19 @@
+import { Link } from "@tanstack/react-location";
+
 interface HomeProps {}
 
 function Home(props: HomeProps) {
-  return <div>Home</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="posts" search={{ page: 1, limit: 10 }}>
+            Link to posts with query params
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
